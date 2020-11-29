12d2b1156d7774337c318a2e6ba0c7257061ddea
akinghill <austinkinghill@gmail.com>
2020-11-29 15:08:46 -0700

    pokemon page

--- src/pages/pokemon.page.jsx
+++ src/pages/pokemon.page.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
-import { Typography } from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+import { Button, Link, CircularProgress, Typography } from '@material-ui/core'
 import mockdata from '../mockdata'
 import { makeStyles } from '@material-ui/core/styles'
-
-
+import axios from 'axios'
 
 const useStyles = makeStyles({
   cardText: {
@@ -12,26 +11,67 @@ const useStyles = makeStyles({
 })
 
 const Pokemon = (props) => {
+  const { match, history } = props
   const classes = useStyles()
-  const { match } = props
   const { params } = match
   const { pokemonId } = params
-  const [pokemon, setPokemon] = useState(mockdata[`${pokemonId}`])
+  const [pokemon, setPokemon] = useState(undefined)
 
-  const generatePokemonPage = () => {
-    const { name, id, species, height, weight, types, sprites } = pokemon
-    const fullImageUrl = ``
-    const { front_default } = sprites
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
+      .then(res => {
+        const { data } = res;
+        setPokemon(data)
+      })
+      .catch(err => setPokemon(false))
+  }, [pokemonId])
+
+  const generatePokemonPage = (pokemon) => {
+    const { name, id, species, height, weight, types } = pokemon
+    const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`
+    // const {sprites} = pokemon
+    // const { front_default } = sprites
 
     return (
-      <Typography className={classes.cardText} variant="h1">
-        {`${id}. ${name}`}
-      </Typography>
+      <>
+        <Typography className={classes.cardText} variant="h1">
+          {`${id}. ${name}`}
+        </Typography>
+        <img style={{ width: '300px', height: '300px' }} src={fullImageUrl} alt="" />
+        <Typography variant="h3">Pokemon Info</Typography>
+        <Typography>
+          {"Species: "}
+          <Link href={species.url}>{species.name}</Link>
+        </Typography>
+        <Typography>Height: {height}</Typography>
+        <Typography>Weight: {weight}</Typography>
+        <Typography variant="h6"> Types: </Typography>
+        {types.map((typeInfo) => {
+          const { type } = typeInfo
+          const { name } = type
+          return <Typography key={name}> {`${name}`} </Typography>
+        })}
+      </>
     )
 
   }
 
-  return <> {generatePokemonPage()} </>
+  return (
+    <>
+      {pokemon === undefined && <CircularProgress />}
+      {pokemon !== undefined && pokemon && generatePokemonPage(pokemon)}
+      {pokemon === false && <Typography> Pokemon not found </Typography>}
+
+      {
+        pokemon !== undefined && (
+          <Button variant="contained" onClick={() => history.push('/')}>
+            back to pokedex
+          </Button>
+        )
+      }
+    </>
+  )
 }
 
 export default Pokemon
